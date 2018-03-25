74311b618f41b4493d7a3fd8ccd3e8da03f8f0b2
Rob <dev.rcc@gmail.com>
2018-03-25 15:00:25 -0600

    Integración de la función executeCURL en la líbreria, para facilitar la consulta

--- app/libs/Helper.php
+++ app/libs/Helper.php
@@ -20,7 +20,20 @@ class Helper{
         $text2url = str_replace(' ','-',trim($text2url));
         return $text2url;
     }
-    
+
+    public static function executeCURL($url, $campos){
+        foreach($campos as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+        rtrim($fields_string, '&');
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_POST, count($campos));
+        curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($ch);
+        curl_close($ch);
+        return $respuesta;
+    }
+
     public static function renderHTMLtoRichtext($descripcion){
         $contenido_html = str_replace('</p>', '</p><br>', $descripcion);
         $contenido_html = str_replace('strong', 'b', $contenido_html);
