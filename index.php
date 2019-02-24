e9a692404956af25c471853bc5352ff6c46da529
Rob <dev.rcc@gmail.com>
2019-02-24 02:39:21 +0100

    Se ha implementado las rutas para urls más 'bonitas'

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    ini_set('display_errors', 1);
     try{
         if (file_exists('vendor/autoload.php')) {
             require 'vendor/autoload.php';
@@ -11,10 +12,10 @@
         require 'config/class_alias.php';
         require APP . 'libs/Helper.php';
         require APP . 'core/Core.php';
-        require APP . 'core/App.php';
         require APP . 'core/Controller.php';
         require APP . 'core/Database.php';
         require APP . 'core/View.php';
+        require 'routes.php';
 
         spl_autoload_register(function ($class) {
             $file = MODEL .$class.'.php';
@@ -22,8 +23,6 @@
                 require_once $file;
             }
         });
-
-        $app = new App();
     } catch (Exception $ex){
         echo $ex->getMessage();
         header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
---
+++ routes.php
@@ -0,0 +1,33 @@
+<?php
+    $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+        $r->addRoute('GET', '/', 'DefaultController@index');
+    });
+
+    $httpMethod = $_SERVER['REQUEST_METHOD'];
+    $uri = $_SERVER['REQUEST_URI'];
+
+    if (false !== $pos = strpos($uri, '?')) {
+        $uri = substr($uri, 0, $pos);
+    }
+
+    $uri = rawurldecode($uri);
+    $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+    switch ($routeInfo[0]) {
+        case FastRoute\Dispatcher::NOT_FOUND:
+            echo '404';
+            break;
+
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            $allowedMethods = $routeInfo[1];
+            echo  'no allowed';
+            break;
+
+        case FastRoute\Dispatcher::FOUND:
+            $handler = $routeInfo[1];
+            $vars = $routeInfo[2];
+            list($class, $method) = explode("@", $handler, 2);
+            require_once APP . 'controller/' . $class . '.php';
+            call_user_func_array(array(new $class, $method), $vars);
+        break;
+    }
